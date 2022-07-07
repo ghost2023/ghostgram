28406ffbdde69abf1652caf89e285c93a474a4d2
ghost2023 <nathnaelwonisha@gmail.com>
2022-07-07 18:42:39 +0300

    created home page and add to route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter,Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter> 
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import App from './App'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
