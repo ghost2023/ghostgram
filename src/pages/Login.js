af91d8b7f53da05860a35bc46c8aab767dcdeb5e
ghost2023 <nathnaelwonisha@gmail.com>
2022-07-13 11:15:28 +0300

    fixed login issue

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ export default function Login() {
 
   function loginSub(e){
     e.preventDefault()
-    signInWithEmailAndPassword(getAuth, email, password).then(() => {
+    signInWithEmailAndPassword(getAuth(), email, password).then(() => {
       nav('/')
     })
   }
