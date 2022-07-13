bb101a9fe4e90ab7d4dd9763f0475eeb66132740
ghost2023 <nathnaelwonisha@gmail.com>
2022-07-13 18:19:41 +0300

    able to hide the accoun dropdown menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import NewPost from './NewPost'
 import { Link, useNavigate } from 'react-router-dom'
 import img from '../assets/logo.png'
 import PP from '../assets/default_profile.png';
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getAuth, signOut } from 'firebase/auth';
 import { get, ref as dbRef } from 'firebase/database';
 import { DB } from '../fb-config'
@@ -16,8 +16,11 @@ export default function NavBar({ page }) {
   const user = getAuth()
   const [pPic, setPPic] = useState(PP)
   const [profile, setProfile] = useState({})
-  console.log(user)
+  const [isFDOpen, setFDOpen] = useState(false)
+  const [isDBOpen, setDBOpen] = useState(false)
+  
   useEffect(() => {
+    console.log(user)
     if(!user.currentUser) nav('/login')
     get(dbRef(DB, 'users/' + user.uid)).then(dat => {
       const data = dat.val()
@@ -38,13 +41,13 @@ export default function NavBar({ page }) {
           <Search/> 
         </div>
         <div className={s.panel}>
-          <HomeBtn page={page}/> 
+          <HomeBtn page={page} isOpen={isDBOpen || isFDOpen}/> 
           <DMBtn page={page}/> 
           <PostBtn open={() => openNewPost(true)}/> 
           <ExploreBtn page={page}/> 
-          <ActivityBtn/> 
-          <div className={s.account}>
-            <div className={s.icon}>
+          <ActivityBtn isOpen={isFDOpen} open={() => setFDOpen(true)}/> 
+          <div className={s.account + (isDBOpen ? ' ' + s.foc: '')}>
+            <div className={s.icon} onClick={() => setDBOpen(true)}>
               <img src={pPic} alt="" />
             </div>
             <div className={s.dropdown}>
@@ -86,13 +89,14 @@ export default function NavBar({ page }) {
           </div> 
         </div>
       </div>
+      <div className={s.hider + (isDBOpen || isFDOpen ? ` ${s.open}` : '' )} onClick={() => {setFDOpen(false);setDBOpen(false)}}></div>
       {isNewPostOpen?<NewPost closeNewPost={() => openNewPost(false)}/>:<></>}
     </div>
   )
 }
 
-function HomeBtn({ page }) {
-  let svgPath = page == "Home"? <path d="M22 23h-6.001a1 1 0 01-1-1v-5.455a2.997 2.997 0 10-5.993 0V22a1 1 0 01-1 1H2a1 1 0 01-1-1V11.543a1.002 1.002 0 01.31-.724l10-9.543a1.001 1.001 0 011.38 0l10 9.543a1.002 1.002 0 01.31.724V22a1 1 0 01-1 1z"></path> : <path d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path>
+function HomeBtn({ page, isOpen }) {
+  let svgPath = page == "Home" || !isOpen? <path d="M22 23h-6.001a1 1 0 01-1-1v-5.455a2.997 2.997 0 10-5.993 0V22a1 1 0 01-1 1H2a1 1 0 01-1-1V11.543a1.002 1.002 0 01.31-.724l10-9.543a1.001 1.001 0 011.38 0l10 9.543a1.002 1.002 0 01.31.724V22a1 1 0 01-1 1z"></path> : <path d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path>
   
   return (
     <Link to='/' className={s['panel-btn']}>
@@ -139,13 +143,14 @@ function ExploreBtn({page}) {
   )
 }
 
-function ActivityBtn() {
+function ActivityBtn({ isOpen, open }) {
+  const path = isOpen? "M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z" : "M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"
   return (
-    <div className={s['panel-btn']}>
-
-    <svg>
-      <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path>
-    </svg>
+    <div className={s['panel-btn'] + ' ' + s.activity + (isOpen ? ` ${s.foc}`: '')}>
+      <svg viewBox={isOpen? '0 0 48 48': '0 0 24 24'} onClick={open}>
+        <path d={path}></path>
+      </svg>
+      <div className={s.dropdown}></div>
     </div>
   )
 }
