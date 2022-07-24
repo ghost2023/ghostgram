c88e527a3b92c19955b19c1a998c8047e182e7d6
ghost2023 <nathnaelwonisha@gmail.com>
2022-07-24 21:18:34 +0300

    created userContext.js

---
+++ src/userContext.js
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+import { auth, DB, SG } from './fb-config'
+import { get, ref as dbRef } from "firebase/database";
+import { ref, getDownloadURL } from "firebase/storage";
+
+const userContext = createContext()
+
+export function useAuth(){
+    return useContext(userContext)
+}
+
+export default function userAuthProvider({ children }) {
+    const [user, setUser] = useState({});
+
+    function login(email, password) {
+      return signInWithEmailAndPassword(auth, email, password);
+    }
+    async function signUp(name, username, email, password) {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const uid = userCredential.user.uid
+      set(ref(DB, 'users/' + uid), {
+        name,
+        email,
+        username,
+        profile: 'default-avatar.jpg',
+      })
+    }
+    function logOut() {
+      return signOut(auth);
+    }
+    async function getProfileURL(){
+      const imgRef = ref(SG, 'profiles/' + user.profile)
+      return await getDownloadURL(SG, imgRef)
+    }
+    function getFollowers(){
+
+    }
+    function getFollowing(){
+
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentuser) => {
+          console.log("Auth", currentuser);
+          const userRef  = dbRef(DB, `users/${currentuser?.user}`)
+          get(userRef).then((data) => { 
+            setUser({...currentuser, ...data.val()});
+          })
+        });
+    
+        return () => unsubscribe();
+      }, []);
+
+  return (<userContext.Provider value={{login, signUp, logOut}}>
+      {children}
+    </userContext.Provider>)
+}
