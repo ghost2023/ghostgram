1413b05c3aea270b567f7d36f8f084855b295a4d
ghost2023 <nathnaelwonisha@gmail.com>
2022-07-24 21:28:53 +0300

    imported and used UserAuthProvider

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import { BrowserRouter,Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import UserAuthProvider from "./userContext";
 
 function App() {
   return (
     <BrowserRouter> 
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-      </Routes>
+      <UserAuthProvider>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+      </UserAuthProvider>
     </BrowserRouter>
   );
 }
