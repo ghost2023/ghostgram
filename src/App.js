7ad2fc1f36d94e7dc03c430d5275ff6c178ccc88
ghost2023 <nathnaelwonisha@gmail.com>
2022-07-25 13:40:16 +0300

    created privateRoute for home and login page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import { BrowserRouter,Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import UserAuthProvider from "./userContext";
+import { useAuth } from "./userContext";
 
 function App() {
   return (
     <BrowserRouter> 
       <UserAuthProvider>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
+          
+          <Route path='/' element={
+            <PrivateRoute/>
+          }/>
           <Route path='/signup' element={<SignUp/>}/>
         </Routes>
       </UserAuthProvider>
     </BrowserRouter>
   );
+}
+
+function PrivateRoute({  }) {
+  const { user } = useAuth()
+  console.log(user)
+  if (!user) {
+    return <Login/>;
+  }
+  return <Home/>;
 }
 
 export default App;
