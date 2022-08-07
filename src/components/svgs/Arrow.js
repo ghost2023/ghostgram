5b0c462d19d1a566aa9f3dabd2cf97ca635a4330
ghost2023 <nathnaelwonisha@gmail.com>
2022-08-07 11:33:09 +0300

    created Arrow.js

---
+++ src/components/svgs/Arrow.js
@@ -0,0 +1,8 @@
+export default function Arrow({ dxn }) {
+  return (
+    <svg height="24" role="img" viewBox="0 0 24 24" width="24">
+        <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="2.909" x2="22.001" y1="12.004" y2="12.004"></line>
+        <polyline fill="none" points="9.276 4.726 2.001 12.004 9.276 19.274" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></polyline>
+    </svg>
+  )
+}
