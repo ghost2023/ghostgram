8ef0ec630d06e530100102c61c07474f48af5143
ghost2023 <nathnaelwonisha@gmail.com>
2022-08-07 11:38:41 +0300

    added sliding capability to slider.js fixed indicators

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,30 +1,37 @@
-import { useEffect, useState } from 'react'
+import { useRef, useState } from 'react'
 import s from '../styles/Slider.module.css'
 import CircleChevron from './svgs/CircleChevron'
 
-export default function slider({ children }) {
+export default function Slider({ children, isInPost }) {
   const len = children.length
   const [page, setPage] = useState(0)
-  let indicators = []
-  useEffect(() => {
-    indicators = []
-    for(let i = 0; i < len; i++){
-      let style = s.indicator
-      if(i === page) style += ` ${s.current}`
-      indicators.push(<div className={style}></div>)
-    }
-  }, [page])
+  const container = useRef()
+  let width = container.current?.offsetWidth / len
+
+  function goLeft(){
+    if(page === 0) return
+    setPage(p => p - 1)    
+  }
+  function goRight(){
+    if(page === len - 1) return
+    setPage(p => p + 1)    
+  }
   return (
     <div className={s.slider}>
-      <div className={s.container} style={{width: `${len * 100}%` }}>
+      <div className={s.hider}>
+        <div className={s.container} ref={container} style={{width: `${len * 100}%`, transform: `translateX(-${width * page}px)`}}>
 
-        {children.map(child => (
-          <div className={s.slide}>{child}</div>
-        ))}
+          {children.map((child, i) => (
+            <div className={s.slide} key={i}>{child}</div>
+            ))}
+        </div>
       </div>
-      <div className={`${s.left} ${s.btn}`}><CircleChevron/></div>
-      <div className={`${s.right} ${s.btn}`}><CircleChevron/></div>
-      <div className={s.indicators}>{indicators}</div>
+      {page === 0 || <div onClick={goLeft} className={`${s.left} ${s.btn}`}><CircleChevron/></div>}
+      {page === len - 1 || <div onClick={goRight} className={`${s.right} ${s.btn}`}><CircleChevron/></div>}
+      <div className={s.indicators + (isInPost ? ` ${s.bottom}` : '')}>
+        {[...Array(len)].map((_, i) =>
+          <div key={i}  className={s.indicator + (i === page ? ` ${s.current}` : '')}/>
+        )}</div>
     </div>
   )
 }
