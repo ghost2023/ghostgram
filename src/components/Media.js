ce1d2e358de3b3f9eeb8b0bc314e61087da07ed6
ghost2023 <nathnaelwonisha@gmail.com>
2022-08-07 11:32:45 +0300

    created media.js

---
+++ src/components/Media.js
@@ -0,0 +1,12 @@
+import { getDownloadURL, ref } from "firebase/storage"
+import { useEffect, useState } from "react"
+import { SG } from "../fb-config"
+
+export default function Media({ path }) {
+    const [url, setUrl] = useState('')
+
+    useEffect(() => {
+        getDownloadURL(ref(SG, path)).then(u => setUrl(u))
+    }, [])
+  return path.endsWith('.jpg')? <img src={url}></img> : <video src={url} controls></video>
+}
