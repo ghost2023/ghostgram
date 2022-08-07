70db10bf03ccb7096e246b53bf0310510323a359
ghost2023 <nathnaelwonisha@gmail.com>
2022-08-07 11:36:06 +0300

    made Oprions.js resizable

--- src/components/svgs/Options.js
+++ src/components/svgs/Options.js
@@ -1,6 +1,7 @@
-export default function Options() {
+export default function Options({isSmall}) {
+  const sz = isSmall? '24px': '32px'
   return (
-    <svg aria-label="Options" height="32" role="img" viewBox="0 0 24 24" width="32">
+    <svg aria-label="Options" height={sz} role="img" viewBox="0 0 24 24" width={sz}>
         <circle cx="12" cy="12" r="1.5"></circle>
         <circle cx="6" cy="12" r="1.5"></circle>
         <circle cx="18" cy="12" r="1.5"></circle>
