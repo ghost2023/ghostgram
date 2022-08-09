0aabf7e7e04586e1aa3e88c221b23ec9642e7898
ghost2023 <nathnaelwonisha@gmail.com>
2022-08-09 20:45:10 +0300

    created warning.js

---
+++ src/components/Warning.js
@@ -0,0 +1,11 @@
+import Overlay from './Overlay'
+
+export default function Warning({ close, msg, btn }) {
+  return (
+    <Overlay onClick={close}>
+        <p>{msg}</p>
+        {btn}
+      <button className=""></button>
+    </Overlay>
+  )
+}
