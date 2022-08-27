2516a5ef8d4d0dd3eadb9f4634a8b960c8963db4
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-08-27 17:37:08 +0300

    changed user id by username fixed issues in userContext

--- src/userContext.js
+++ src/userContext.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { equalTo, get, orderByChild, push, query, ref as dbRef, remove, set, update } from "firebase/database";
+import { equalTo, get, orderByChild, push, query, ref as dbRef, remove, set } from "firebase/database";
 import { getDownloadURL, ref } from "firebase/storage";
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth, DB, SG } from './fb-config';
@@ -14,16 +14,16 @@ export default function UserAuthProvider({ children }) {
     const [user, setUser] = useState();
     const [follows, setFollows] = useState([]);
     const [isLoading, setLoading] = useState(true);
-
+    
     function login(email, password) {
       return signInWithEmailAndPassword(auth, email, password);
     }
     async function signUp(name, username, email, password) {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      set(dbRef(DB, 'users/' + userCredential.user.uid), {
+      set(dbRef(DB, 'users/' + username), {
         name,
         email,
-        username,
+        uid : userCredential.user.uid,
         profile: 'default-avatar.jpg',
         occupation: '',
         bio: '',
@@ -35,34 +35,30 @@ export default function UserAuthProvider({ children }) {
     function logOut() {
       return signOut(auth);
     }
-    async function follow(uid){
-      if(follows.some(i => i[1] === uid)) return
+    async function follow(username){
+      if(follows.some(i => i[1] === username)) return
       const followRef = push(dbRef(DB, 'follows/'))
       await set(followRef, {
-        follower: user.uid,
-        followe: uid
+        follower: user.username,
+        followe: username
       })
       updateFollow()
     }
-    async function unFollow(uid){
-      const followOb = follows.find(i => i[1] === uid) 
+    async function unFollow(username){
+      const followOb = follows.find(i => i[1] === username) 
       if(!followOb) return
       const followRef = dbRef(DB, 'follows/' + followOb[0])
       await remove(followRef)
       updateFollow()
     }
-    async function updateFollow(uid = user.uid){
-      const followsQuery = query(dbRef(DB, 'follows/'), orderByChild('follower'), equalTo(uid))
+    async function updateFollow(username = user.username){
+      const followsQuery = query(dbRef(DB, 'follows/'), orderByChild('follower'), equalTo(username))
       const followsData = (await get(followsQuery)).val()
       if(!followsData){
-        await update(dbRef(DB, `users/${uid}`), {followings: 0})
-        setUser(p => {return {...p, followings: 0}})
         setFollows([])
         return
       }
       const followsArr = Object.entries(followsData).map(([id, body]) => [id, body.followe])
-      await update(dbRef(DB, `users/${uid}`), {followings: followsArr.length})
-      setUser(p => {return {...p, followings: followsArr.length}})
       setFollows(followsArr)
     }
 
@@ -73,17 +69,23 @@ export default function UserAuthProvider({ children }) {
             setLoading(false)
             return console.log('no user')
           }
-          const userRef  = dbRef(DB, `users/${currentuser?.uid}`)
-          get(userRef).then(async (data) => {
-            console.log("Auth", {...currentuser, ...data.val()});
+          const userQuery  = query(dbRef(DB, `users/`), orderByChild('uid'), equalTo(currentuser.uid))
+          get(userQuery).then(d => {
+            const username = Object.keys(d.val())[0]
+
+            const userObj = {
+              ...currentuser,
+              username,
+              ...Object.values(d.val())[0]
+            }
+            console.log("Auth", username);
+
 
             // get and store profile image Url
-            await updateFollow(currentuser.uid)
-
-            getDownloadURL(ref(SG, `profiles/${data.val().profile}`)).then(url => {
-              setUser({...currentuser, ...data.val(), profile: url});
-              setLoading(false)
-            })
+            updateFollow(username).then(() => setLoading(false))
+                        
+            getDownloadURL(ref(SG, `profiles/${userObj.profile}`))
+              .then(u => setUser({...userObj, profileURL: u}))
           })
         });
         
