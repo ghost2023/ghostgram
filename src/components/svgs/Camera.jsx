2a6ee8f63da3b0bb6dab0dc420a1c9f319a6ccb4
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-09-17 16:57:37 +0300

    created Camera Table Tag svg

---
+++ src/components/svgs/Camera.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Camera() {
+  return (
+    <svg aria-label="Comment" height="24" role="img" width="24px" viewBox="0 0 32 32">
+        <path d="M23,9l-1.4-2.9c-0.3-0.7-1-1.1-1.8-1.1h-7.5c-0.8,0-1.5,0.4-1.8,1.1L9,9H5c-1.1,0-2,0.9-2,2v14c0,1.1,0.9,2,2,2  h22c1.1,0,2-0.9,2-2V11c0-1.1-0.9-2-2-2H23z" fill="none" stroke="currentColor"  strokeLinejoin="round"  />
+        <circle cx="16" cy="17" r="6" fill="none" stroke="currentColor"  strokeLinejoin="round" />
+    </svg>
+  )
+}
---
+++ src/components/svgs/Table.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Table() {
+  return (
+    <svg fill="#8e8e8e" height="12" role="img" viewBox="0 0 24 24" width="12">
+        <rect fill="none" height="18" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" width="18" x="3" y="3"></rect>
+        <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="9.015" x2="9.015" y1="3" y2="21"></line>
+        <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="14.985" x2="14.985" y1="3" y2="21"></line>
+        <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="21" x2="3" y1="9.015" y2="9.015"></line>
+        <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="21" x2="3" y1="14.985" y2="14.985"></line>
+    </svg>
+  )
+}
