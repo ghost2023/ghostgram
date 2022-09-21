77600a0aa7a5d2c0d4ff4ed40a4ba383b8a314d9
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-09-21 22:11:34 +0300

    finished UnfollowModal and styled it

---
+++ src/components/UnfollowModal.jsx
@@ -0,0 +1,42 @@
+import { get, ref } from 'firebase/database'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { DB } from '../fb-config'
+import style from '../styles/UnfollowModal.module.css'
+import { useAuth } from '../userContext'
+import Media from './Media'
+import Overlay from './Overlay'
+
+export default function UnfollowModal({ username, onUnfollow, closeModal }) {
+  const { unFollow } = useAuth()
+  const [profile, setProfile] = useState('')
+
+  useEffect(() => {
+    get(ref(DB, `users/${username}/profile`))
+      .then(snapShot => {
+        setProfile(snapShot.val())
+      })
+  }, [username])
+
+  if(!profile) return null
+  return (
+    <Overlay onClick={closeModal}>
+      <div className={style.modal}>
+        <div className={style.profile}>
+          <Link to={`/${username}`}>
+            <Media path={`profiles/${profile}`}/>
+          </Link>
+        </div>
+        
+        <div className={style.username}>
+            Leave {username}?
+        </div>
+
+        <div className={style.btns}>
+          <button onClick={() => {unFollow(username);onUnfollow()}} className={style.unfollow}>Unfollow</button>
+          <button onClick={closeModal}>Cancel</button>
+        </div>
+      </div>
+    </Overlay>
+  )
+}
