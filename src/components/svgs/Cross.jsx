aad5ae22f9cec0129d2b9907de8febe08ceea7bb
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-09-21 19:39:03 +0300

    extracted Cross.jsx svg

---
+++ src/components/svgs/Cross.jsx
@@ -0,0 +1,11 @@
+export default function Cross(small) { 
+    const size = small?"18": "48" 
+    return (
+        <svg viewBox="0 0 24 24" width={size} height={size} >
+            <title>Close</title>
+            <polyline fill="none" points="20.643 3.357 12 12 3.353 20.647" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3"></polyline>
+            <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" x1="20.649" x2="3.354" y1="20.649" y2="3.354"></line>
+        </svg>
+    )
+
+}
