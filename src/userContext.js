157096a257390013f3a32d33024d848c5183aa5e
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-09-21 22:13:14 +0300

    optimized following & unfollowing

--- src/userContext.js
+++ src/userContext.js
@@ -1,5 +1,5 @@
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
-import { equalTo, get, orderByChild, push, query, ref as dbRef, remove, set } from "firebase/database";
+import { equalTo, get, orderByChild, push, query, ref as dbRef, remove, set, update } from "firebase/database";
 import { getDownloadURL, ref } from "firebase/storage";
 import { createContext, useContext, useEffect, useState } from "react";
 import { auth, DB, SG } from './fb-config';
@@ -37,29 +37,42 @@ export default function UserAuthProvider({ children }) {
       return signOut(auth);
     }
     async function follow(username){
-      if(follows.some(i => i[1] === username)) return
+      if(follows.some(i => i.user === username)) return
       const followRef = push(dbRef(DB, 'follows/'))
       await set(followRef, {
         follower: user.username,
         followe: username
       })
-      updateFollow()
+      const newFollowingCount = user.followings + 1
+      await update(dbRef(DB, `users/${user.username}/`), {followings: newFollowingCount})
+      setFollows(prev => [...prev, {id:followRef.key, user:username}])
+      setUser(prev => {
+        return { ...prev, followings: newFollowingCount}
+      })
     }
     async function unFollow(username){
-      const followOb = follows.find(i => i[1] === username) 
+      const followOb = follows.find(i => i.user === username) 
       if(!followOb) return
-      const followRef = dbRef(DB, 'follows/' + followOb[0])
+      const followRef = dbRef(DB, 'follows/' + followOb.id)
       await remove(followRef)
-      updateFollow()
+      const newFollowingCount = user.followings - 1
+      await update(dbRef(DB, `users/${user.username}/`), {followings: newFollowingCount})
+      setFollows(prev => {
+        const newArr = prev.filter(item => item.username === username)
+        return newArr
+      })
+      setUser(prev => {
+        return {...prev, followings: newFollowingCount}
+      })
     }
-    async function updateFollow(username = user.username){
+    async function getFollowing(username){
       const followsQuery = query(dbRef(DB, 'follows/'), orderByChild('follower'), equalTo(username))
       const followsData = (await get(followsQuery)).val()
       if(!followsData){
         setFollows([])
         return
       }
-      const followsArr = Object.entries(followsData).map(([id, body]) => [id, body.followe])
+      const followsArr = Object.entries(followsData).map(([id, body]) => {return {id,user:body.followe}})
       setFollows(followsArr)
     }
 
@@ -83,7 +96,7 @@ export default function UserAuthProvider({ children }) {
           setUser(userObj)
 
           // get and store profile image Url
-          await updateFollow(username)
+          await getFollowing(username)
                       
           const url = await getDownloadURL(ref(SG, `profiles/${userObj.profile}`)) 
           setProfileUrl(url)
