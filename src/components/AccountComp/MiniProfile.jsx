6f556b76a2cf7918b6300c3446b18767fdcb06eb
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-09-22 17:04:39 +0300

    added Unfollowmodal to MiniProfile.jsx - styled it - condn render is logged user is following this user

--- src/components/AccountComp/MiniProfile.jsx
+++ src/components/AccountComp/MiniProfile.jsx
@@ -2,22 +2,27 @@ import { get, ref } from 'firebase/database'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { DB } from '../../fb-config'
-import s from '../../styles/Account.module.css'
+import s from '../../styles/FollowModal.module.css'
 import { useAuth } from '../../userContext'
 import AccountLink from '../AccountLink'
 import Media from '../Media'
+import UnfollowModal from '../UnfollowModal'
 
 export default function MiniProfile({ username }) {
-    const { user, follows } = useAuth()
+    const { follows, follow, user } = useAuth()
     const [userProfile, setUser] = useState()
+    const [modal, setModal] = useState(false)
+    const [isFollow, setFollow] = useState(false)
 
     useState(() => {
+        if(!username) return
         get(ref(DB, `users/${username}/`)).then(snapShot => {
             setUser(snapShot.val())
         })
+        setFollow(follows.some(item => item.user === username))
     }, [username])
 
-    if(!userProfile) return <></>
+    if(!userProfile || username === user.username) return null
   return (
     <div className={s.account}>
         <Link to={`/${username}`}>
@@ -31,7 +36,12 @@ export default function MiniProfile({ username }) {
                 {userProfile?.name}
             </span>
         </div>
-        <button>Follow</button>
+        {isFollow? 
+            <button onClick={() => setModal(true)} className={s['unfollow-btn']}>Following</button>:
+            <button onClick={() => {follow(username); setFollow(true)}}>Follow</button>
+        }
+        {!modal ||
+        <UnfollowModal {...{username}} onUnfollow={() => setFollow(false)} closeModal={() => setModal(false)}/>}
     </div>
   )
 }
