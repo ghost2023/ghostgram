113bec4b578eff6c27d73b614bd49d3ae2004201
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-10-05 17:30:33 +0300

    cleaned userContext

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,7 +1,8 @@
 import { onAuthStateChanged } from 'firebase/auth';
-import { equalTo, get, orderByChild, push, query, ref as dbRef, remove, set, update } from "firebase/database";
+import { push, ref as dbRef, remove, set, update } from "firebase/database";
 import { getDownloadURL, ref } from "firebase/storage";
 import { createContext, useEffect, useState } from "react";
+import { getFollowing, getUserByUid } from 'utils/services';
 import { auth, DB, SG } from '../fb-config';
 
 export const userContext = createContext()
@@ -11,47 +12,44 @@ export default function UserAuthProvider({ children }) {
     const [follows, setFollows] = useState([]);
     const [isLoading, setLoading] = useState(true);
     const [profileUrl, setProfileUrl] = useState('')
+    const value = {
+      user,
+      profileUrl,
+      isLoading,
+      follows,
+      follow,
+      unFollow,
+      setUser,
+      updateProfile
+    }
     
-    async function follow(username){
-      if(follows.some(i => i.user === username)) return
+    async function follow(uid){
+      if(follows.some(i => i.user === uid)) return
       const followRef = push(dbRef(DB, 'follows/'))
       await set(followRef, {
-        follower: user.username,
-        followe: username
+        follower: user.uid,
+        user: uid
       })
-      const newFollowingCount = user.followings + 1
-      await update(dbRef(DB, `users/${user.username}/`), {followings: newFollowingCount})
-      setFollows(prev => [...prev, {id:followRef.key, user:username}])
+      const followings = user.followings + 1
+      await update(dbRef(DB, `users/${user.uid}/`), { followings })
+      setFollows(prev => [...prev, {id:followRef.key, user: uid}])
       setUser(prev => {
-        return { ...prev, followings: newFollowingCount}
+        return { ...prev, followings}
       })
     }
     
-    async function unFollow(username){
-      const followOb = follows.find(i => i.user === username) 
-      if(!followOb) return
-      const followRef = dbRef(DB, 'follows/' + followOb.id)
-      await remove(followRef)
-      const newFollowingCount = user.followings - 1
-      await update(dbRef(DB, `users/${user.username}/`), {followings: newFollowingCount})
+    async function unFollow(uid){
+      const followObj = follows.find(i => i.user === uid) 
+      if(!followObj) return
+      await remove(dbRef(DB, 'follows/' + followObj.id))
+      const followings = user.followings - 1
+      await update(dbRef(DB, `users/${user.uid}/`), { followings })
       setFollows(prev => {
-        const newArr = prev.filter(item => item.username === username)
-        return newArr
+        return prev.filter(item => item.user === uid)
       })
       setUser(prev => {
-        return {...prev, followings: newFollowingCount}
+        return {...prev, followings}
       })
-    }
-
-    async function getFollowing(username){
-      const followsQuery = query(dbRef(DB, 'follows/'), orderByChild('follower'), equalTo(username))
-      const followsData = (await get(followsQuery)).val()
-      if(!followsData){
-        setFollows([])
-        return
-      }
-      const followsArr = Object.entries(followsData).map(([id, body]) => {return {id,user:body.followe}})
-      setFollows(followsArr)
     }
 
     async function updateProfile(newProfile){
@@ -60,36 +58,35 @@ export default function UserAuthProvider({ children }) {
     }
 
     useEffect(() => {
-        // watch for any user changes
-        const unsubscribe = onAuthStateChanged(auth, async (currentuser) => {
-          if (!currentuser){
-            setUser()
-            setLoading(false)
-            return console.log('no user')
-          }
-          const userQuery  = query(dbRef(DB, `users/`), orderByChild('uid'), equalTo(currentuser.uid))
-          const u = await get(userQuery)
-          const username = Object.keys(u.val())[0]
-
-          const userObj = {
-            ...currentuser,
-            username,
-            ...Object.values(u.val())[0]
-          }
-          console.log("Auth", username);
-          setUser(userObj)
-
-          await getFollowing(username)
-          await updateProfile(userObj.profile)
-          
+      // watch for any user changes
+      const unsubscribe = onAuthStateChanged(auth, async (currentuser) => {
+        if (!currentuser){
+          setUser()
           setLoading(false)
-        });
+          return console.log('no user')
+        }
+        const data = await getUserByUid(currentuser.uid)
+
+        const userObj = {
+          ...currentuser,
+          ...data
+        }
+        console.log("Auth", data.uid, data);
+        setUser(userObj)
+
+        const following = await getFollowing(currentuser.uid)
+        setFollows(following)
+
+        await updateProfile(userObj.profile)
         
-        return () => unsubscribe();
+        setLoading(false)
+      });
+      
+      return () => unsubscribe();
       }, []);
 
 
-  return (<userContext.Provider value={{ user, isLoading, follows, follow, unFollow, profileUrl, setUser, updateProfile }}>
+  return (<userContext.Provider {...{value}}>
       {children}
     </userContext.Provider>)
 }
