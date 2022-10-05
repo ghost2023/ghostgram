04a6b7c4e2206d9fc56b407c6334a2e5fa8cf7d1
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-10-05 17:38:51 +0300

    added userAcc state in Account page

--- src/pages/Account/index.jsx
+++ src/pages/Account/index.jsx
@@ -1,6 +1,7 @@
 import NavBar from 'components/NavBar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
+import { getUserByUsername } from 'utils/services';
 import s from './Account.module.css';
 import Header from "./components/Header";
 import Posts from "./components/Posts";
@@ -8,17 +9,23 @@ import Tabs from './components/Tabs';
 
 export default function Account() {
     const { username } = useParams()
+    const [userAcc, setUserAcc] = useState()
     const [tab, setTab] = useState(0)
-    
+  
+    useEffect(() => {
+      getUserByUsername(username).then(setUserAcc)
+    }, [username])
+
+  if(!userAcc) return null
   return (
     <>
-        <NavBar page={'account'}/>
-        <div className={s.page}>
-            <Header username={username}/>
-            <Tabs {...{setTab, tab}}/>
-            <Posts {...{username}}/>
-            <div className={s.stories}></div>
-        </div>
+      <NavBar page={'account'}/>
+      <div className={s.page}>
+          <Header {...{userAcc}}/>
+          <Tabs {...{setTab, tab}}/>
+          <Posts uid={userAcc.uid}/>
+          <div className={s.stories}></div>
+      </div>
     </>
   )
 }
