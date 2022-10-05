2c6f07be57b4ed7907a02b24492f3831c32a7d7b
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-10-05 17:40:19 +0300

    added user state in comment.jsx

--- src/components/Post/Comment.jsx
+++ src/components/Post/Comment.jsx
@@ -1,20 +1,31 @@
 
 import AccountLink from 'components/AccountLink';
 import Media from 'components/Media';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from 'styles/Post.module.css';
+import { getUserByUid } from 'utils/services';
 
 export default function Comment({ comment }) {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    if(!comment) return
+    getUserByUid(comment.user)
+    .then(data => setUser(data))
+  }, [comment])
+
+  if(!user) return null
   return (
     <div className={style.comment}>
       <div className={style.userpic}>  
-        <Link to={`/${comment.username}`}>
-          <Media path={`profiles/${comment.userProfile}`}/>
+        <Link to={`/${user.username}`}>
+          <Media path={`profiles/${user.profile}`}/>
         </Link>
       </div>
       <div className={style["comment-content"]}>
         <span>
-          <AccountLink username={comment.username}/> 
+          <AccountLink username={user.username}/> 
         </span>
           {comment.content}
       </div>
