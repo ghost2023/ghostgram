d63152eaa00c74da1ed5295ca51ad46130606fb6
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-10-05 17:54:43 +0300

    cleaned likesModal

--- src/components/Post/LikesModal.jsx
+++ src/components/Post/LikesModal.jsx
@@ -5,17 +5,25 @@ import { get, ref } from 'firebase/database'
 import { useEffect, useState } from 'react'
 import s from 'styles/Modal.module.css'
 import Cross from 'svgs/Cross'
+import { getUserByUid } from 'utils/services'
 
 export default function LikesModal({ PostId, closeModal }) {
     const [likes, setLikes] = useState([])
 
     useEffect(() => {
-      get(ref(DB, `likes/${PostId}/`)).then(snapShot => {
+      get(ref(DB, `likes/${PostId}/`))
+      .then(async snapShot => {
         if(!snapShot.val()) return
+        if(!snapShot.val().count) return
         const snapShotData = snapShot.val()
         delete snapShotData.count
-        const likesArr = Object.values(snapShotData)
-        setLikes(likesArr)
+        const usersIds = Object.keys(snapShotData)
+        const usersArr = []
+        for(let uid of usersIds){
+          usersArr.append(await getUserByUid(uid))
+        }
+
+        setLikes(usersArr)
       })
     }, [PostId])
 
