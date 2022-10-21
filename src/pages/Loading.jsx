2875eed5a52c649f9450c3f6f51f0c88b6cbaa3c
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-10-21 11:27:50 +0300

    added and styled spinner svg into loading page

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,7 +1,12 @@
+import Spinner from "svgs/Spinner";
+
 export default function Loading() {
   return (
     <div style={{display:"flex", alignItems:"center", justifyContent:"center", height: "100vh", width:"100vw"}}>
       <h1>Loading</h1> 
+      <div style={{height: "32px", width: "32px", margin: "8px"}}>
+        <Spinner/>
+      </div>
     </div>
   )
 }
