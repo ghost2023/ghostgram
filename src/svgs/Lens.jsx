ec8f473f6d6bde7706c9670a2a710dbdb58d06c9
ghost2023 <95122168+ghost2023@users.noreply.github.com>
2022-10-23 22:12:42 +0300

    fixed lens svg

--- src/svgs/Lens.jsx
+++ src/svgs/Lens.jsx
@@ -1,8 +1,9 @@
 
 export default function Lens() {
   return (
-    <svg aria-label="Open media gallery" height="16" role="img" viewBox="0 0 24 24" width="16">
-        <path d="M19 15V5a4.004 4.004 0 0 0-4-4H5a4.004 4.004 0 0 0-4 4v10a4.004 4.004 0 0 0 4 4h10a4.004 4.004 0 0 0 4-4ZM3 15V5a2.002 2.002 0 0 1 2-2h10a2.002 2.002 0 0 1 2 2v10a2.002 2.002 0 0 1-2 2H5a2.002 2.002 0 0 1-2-2Zm18.862-8.773A.501.501 0 0 0 21 6.57v8.431a6 6 0 0 1-6 6H6.58a.504.504 0 0 0-.35.863A3.944 3.944 0 0 0 9 23h6a8 8 0 0 0 8-8V9a3.95 3.95 0 0 0-1.138-2.773Z" fillRule="evenodd"></path>
+    <svg aria-label="Search" height="16" role="img" viewBox="0 0 24 24" width="16">
+      <path d="M19 10.5A8.5 8.5 0 1 1 10.5 2a8.5 8.5 0 0 1 8.5 8.5Z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
+      <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="16.511" x2="22" y1="16.511" y2="22"></line>
     </svg>
   )
 }
